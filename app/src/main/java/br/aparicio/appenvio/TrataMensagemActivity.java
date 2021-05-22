b57e892d27b69e669cd617ed00e3c6796cdf639f
AparicioFermiano <aparicio.empresarial@gmail.com>
2021-05-22 13:58:44 -0300

    Initial commit

---
+++ app/src/main/java/br/aparicio/appenvio/TrataMensagemActivity.java
@@ -0,0 +1,24 @@
+package br.aparicio.appenvio;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TrataMensagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trata_mensagem);
+
+
+
+        Intent intent = getIntent();
+
+        String mensagemRecebida = intent.getStringExtra(Intent.EXTRA_TEXT);
+
+        Toast.makeText(this, "Mensagem Recebida:" +mensagemRecebida,Toast.LENGTH_LONG).show();
+    }
+}
